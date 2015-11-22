18b9be39d47ec629105faf7534c283a7e02cbbed
The1bman <the1bman.bw@gmail.com>
2015-11-22 16:10:58 -0500

    Update Record.java

--- src/Record.java
+++ src/Record.java
@@ -5,40 +5,40 @@ public class Record implements Serializable
 {
 	private Hashtable<String, Field> fields;
 
-  /**
-  Constructor
-  **/
-	public Record()
-	{
+	/**
+  	Constructor
+  	**/
+  	public Record()
+  	{
 		fields = new Hashtable<String, Field>();
-	}
+  	}
 	
-  /**
-  Adds a field to the hashtable
+  	/**
+  	Adds a field to the hashtable
   
-  @param  colName String name of the Column the field is in
-  @param  f       Field to be hashed
-  **/
+  	@param  colName String name of the Column the field is in
+  	@param  f       Field to be hashed
+  	**/
 	public void addField(String colName, Field f)
 	{
 		this.fields.put(colName, f);
 	}
   
-  /**
-  Adds a field to the hashtable
+  	/**
+  	Adds a field to the hashtable
   
-  @param  colName String name of the Column the field is in
-  @return         Field in that Column
-  **/
+  	@param  colName String name of the Column the field is in
+  	@return         Field in that Column
+  	**/
 	public Field getField(String colName)
 	{
-	  if (fields.containsKey(colName))
-    {        	 	
-      return fields.get(colName);   
-    }
-    else
-    {
-      return null;
-    }
-  }
+    		if (fields.containsKey(colName))
+    		{        	 	
+      			return fields.get(colName);   
+    		}
+    		else
+    		{
+      			return null;
+    		}
+  	}
 }
